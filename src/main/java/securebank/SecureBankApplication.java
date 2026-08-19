ce29e739de3aa1f4ae7c8886368599a8f029b356
maheshi96 <maheshi96prem@gmail.com>
2026-08-19 14:00:52 +1000

    changes

---
+++ src/main/java/securebank/SecureBankApplication.java
@@ -0,0 +1,15 @@
+package main.java.securebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the SecureBank application.
+ */
+@SpringBootApplication
+public class SecureBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureBankApplication.class, args);
+    }
+}
