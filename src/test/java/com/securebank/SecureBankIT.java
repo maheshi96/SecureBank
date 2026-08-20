6a241b362536e6d16e73b838f0d0a55a759dd005
maheshi96 <maheshi96prem@gmail.com>
2026-08-20 21:53:31 +1000

    Add integration test for SecureBank

---
+++ src/test/java/com/securebank/SecureBankIT.java
@@ -0,0 +1,15 @@
+package com.securebank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SecureBankIT {
+
+    @Test
+    void secureBankIntegrationTest() {
+        boolean applicationIntegrationSuccessful = true;
+
+        assertTrue(applicationIntegrationSuccessful);
+    }
+}
