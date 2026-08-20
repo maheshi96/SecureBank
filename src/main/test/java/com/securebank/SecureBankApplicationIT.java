1818332094d1b40da162f53b42524a63319109f4
maheshi96 <maheshi96prem@gmail.com>
2026-08-20 12:02:09 +1000

    changes

---
+++ src/main/test/java/com/securebank/SecureBankApplicationIT.java
@@ -0,0 +1,13 @@
+package main.test.java.com.securebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBankApplicationIT {
+
+    @Test
+    void applicationContextLoads() {
+        // Test passes when the complete Spring application starts.
+    }
+}
---
+++ src/main/test/java/com/securebank/SecureBankApplicationTest.java
@@ -0,0 +1,13 @@
+package com.securebank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SecureBankApplicationTest {
+
+    @Test
+    void applicationUnitTest() {
+        assertTrue(true, "Basic SecureBank unit test should pass");
+    }
+}
